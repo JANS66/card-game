3e2bc856c8980f0eb4dac599c4bd26bcc7e52aee
JANS66 <98973687+JANS66@users.noreply.github.com>
2025-12-27 13:07:00 +0200

    Limit to 2 Flipped Cards and Track Flipped Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Card from "./components/Card.jsx"
 import "./App.css"
 
@@ -29,16 +29,57 @@ export default function App() {
   }
 
   const handleClick = id => {
+    if (disableClicks) return
+    if (flippedCards.includes(id)) return
+
+    const newFlipped = [...flippedCards, id]
+
     setCards(previousCards =>
       previousCards.map(card =>
-        card.id === id && !card.isMatched
-          ? {...card, isFlipped: !card.isFlipped}
-          : card
+        card.id === id ? {...card, isFlipped: true} : card 
       )
     )
+
+    setFlippedCards(newFlipped)
+
+    if (newFlipped.length === 2) {
+      setDisableClicks(true)
+      checkForMatch(newFlipped)
+    }
+  }
+
+  const checkForMatch = ([id1, id2]) => {
+    const card1 = cards.find(card => card.id === id1)
+    const card2 = cards.find(card => card.id === id2)
+
+    if (card1.value === card2.value) {
+      setTimeout(() => {
+        setCards(previousCards =>
+          previousCards.map(card =>
+            card.value === card1.value ? {...card, isMatched: true} : card
+          )
+        )
+        setFlippedCards([])
+        setDisableClicks(false)
+      }, 500)
+    } else {
+      setTimeout(() => {
+        setCards(previousCards =>
+          previousCards.map(card =>
+            card.id === id1 || card.id === id2
+              ? {...card, isFlipped: false}
+              : card
+          )
+        )
+        setFlippedCards([])
+        setDisableClicks(false)
+      }, 1000)
+    }
   }
 
   const [cards, setCards] = React.useState(() => shuffleArray(generateCards()))
+  const [flippedCards, setFlippedCards] = useState([])
+  const [disableClicks, setDisableClicks] = useState(false)
 
   return (
     <div>
